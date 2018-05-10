083d0672f91af35327a5f2b7aeadab6efcc1c425
Brian Stack <bstack@mozilla.com>
2018-05-10 16:32:51 -0700

    Stop compiling

--- src/send.js
+++ src/send.js
@@ -1,10 +1,8 @@
 let assert  = require('assert');
-let urljoin = require('url-join');
 let got     = require('got');
 let debug   = require('debug')('statsum');
 let tcUrl   = require('taskcluster-lib-urls');
 let uuid    = require('uuid');
-let Promise = require('promise');
 
 let contentType = 'application/json';
 let serialize   = (data) => JSON.stringify(data);
